f5955b40c57ecb18377ce8f0611419f727381cd7
Prasetya <prasetya.career@gmail.com>
2026-03-03 10:52:51 +0700

    m

--- app/__init__.py
+++ app/__init__.py
@@ -1,23 +1,14 @@
 from flask import Flask
-from werkzeug.middleware.proxy_fix import ProxyFix
 
 def create_app():
     # initialze core
     core = Flask(__name__, static_folder='static', template_folder='templates')
-
-    # 1️⃣ Terapkan ProxyFix untuk Traefik
-    core.wsgi_app = ProxyFix(
-        core.wsgi_app,
-        x_for=1,     # IP client asli
-        x_proto=1,   # scheme (http/https)
-        x_host=1     # host
-    )
     
-    # register config
+    # CONFIG
     from config import register_config
     register_config(core)
 
-    # register routes
+    # ROUTES
     from app.routes import register_routes
     register_routes(core)
 
